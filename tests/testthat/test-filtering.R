b9575fc58e0c8805103e0fa3a3a615e7d12b2bb1
andreaskapou <kapouranis.andreas@gmail.com>
2019-02-20 18:10:31 +0000

    Add unit tests

---
+++ tests/testthat/test-filtering.R
@@ -0,0 +1,41 @@
+context("Filtering correctly genomic regions")
+
+test_that("Filter by CpG coverage works", {
+  obj <- melissa_encode_dt
+  filt_obj <- filter_by_cpg_coverage(obj, min_cpgcov = 20)
+  expect_equal(sum(sapply(filt_obj$met[[1]], function(n) is.na(n[[1]]))), 43)
+  expect_equal(filt_obj$met[[1]][[100]], NA)
+  expect_error(filter_by_cpg_coverage(obj, min_cpgcov = -1))
+
+  filt_obj <- filter_by_cpg_coverage(obj, min_cpgcov = 1000)
+  expect_equal(sum(sapply(filt_obj$met[[1]], function(n) is.na(n[[1]]))),
+               NROW(obj$met[[1]]))
+})
+
+test_that("Filter by coverage across cells works", {
+  obj <- melissa_encode_dt
+  filt_obj <- filter_by_coverage_across_cells(obj, min_cell_cov_prcg = 0.2)
+  expect_equal(sum(sapply(filt_obj$met[[1]], function(n) is.na(n[[1]]))), 0)
+  expect_error(filter_by_coverage_across_cells(obj, min_cell_cov_prcg = -1))
+  expect_error(filter_by_coverage_across_cells(obj, min_cell_cov_prcg = 2))
+})
+
+test_that("Filter by variability works", {
+  obj <- melissa_encode_dt
+  filt_obj <- filter_by_variability(obj, min_var = 0.1)
+  expect_equal(length(filt_obj$met[[1]]), 96)
+
+  filt_obj <- filter_by_variability(obj, min_var = 0.15)
+  expect_equal(length(filt_obj$met[[1]]), 4)
+
+  expect_error(filter_by_variability(obj, min_var = -1))
+
+  filt_obj <- filter_by_variability(obj, min_var = 1000)
+  expect_equal(length(filt_obj$met[[1]]), 0)
+})
+
+test_that("log sum exp function works", {
+  x <- c(0.001, 0.5, 2, 1.4, 1.5)
+  expect_gt(log_sum_exp(x), 2.92)
+  expect_lt(log_sum_exp(x), 2.93)
+})
---
+++ tests/testthat/test-running-melissa.R
@@ -0,0 +1,81 @@
+context("Checking correct functionality of Melissa")
+
+test_that("Melissa runs and returns correct results", {
+  set.seed(1)
+  dt <- melissa_synth_dt
+  # Partition to train and test set
+  expect_error(partition_dataset(dt, data_train_prcg = 1.4))
+  expect_error(partition_dataset(dt, data_train_prcg = -1))
+  expect_error(partition_dataset(dt, region_train_prcg = 1.4))
+  expect_error(partition_dataset(dt, region_train_prcg = -1))
+
+  dt <- partition_dataset(dt, data_train_prcg = 0.5, region_train_prcg = 0.95,
+                           cpg_train_prcg = 0.5, is_synth = FALSE)
+  expect_equal(NROW(dt$met[[1]][[1]]), 20)
+  expect_equal(NROW(dt$met[[1]][[12]]), 21)
+
+  # Create basis object from BPRMeth package
+  basis_obj <- BPRMeth::create_rbf_object(M = 3)
+  # Run Melissa VB model
+  melissa_obj <- melissa(X = dt$met, K = 2, basis = basis_obj,
+                         delta_0 = NULL, alpha_0 = 0.5, vb_max_iter = 5,
+                         vb_init_nstart = 1, is_parallel = FALSE,
+                         is_verbose = FALSE)
+
+  # Check lots of parameters from Melissa output that match expectations
+  expect_gt(melissa_obj$W[1,1,1], 1.13)
+  expect_lt(melissa_obj$W[1,1,1], 1.136)
+
+  expect_lt(melissa_obj$r_nk[1,2], 1e-20)
+
+  expect_lt(melissa_obj$delta[1], 14)
+  expect_lt(melissa_obj$alpha[1], 61)
+  expect_lt(melissa_obj$beta[1], 37.99)
+  expect_lt(melissa_obj$lb[2], -3426.022)
+  expect_gt(melissa_obj$lb[2], -3426.03)
+
+  # Check imputation performance is correct
+  imputation_obj <- impute_met_state(obj = melissa_obj, test = dt$met_test)
+  expect_lt(imputation_obj$pred_obs[1], 0.515)
+  expect_gt(imputation_obj$pred_obs[1], 0.514)
+  expect_lt(melissa_obj$lb[2], -3426.022)
+
+  # Check evaluation of imputation performance is correct
+  melissa_obj <- eval_imputation_performance(obj = melissa_obj,
+                                             imputation_obj = imputation_obj)
+
+  expect_lt(melissa_obj$imputation$auc, 0.9172)
+  expect_gt(melissa_obj$imputation$auc, 0.9171)
+
+  expect_lt(melissa_obj$imputation$f_measure, 0.86812)
+  expect_gt(melissa_obj$imputation$f_measure, 0.86811)
+
+  expect_lt(melissa_obj$imputation$pr@y.values[[1]][2], 0.85715)
+  expect_gt(melissa_obj$imputation$pr@y.values[[1]][2], 0.85714)
+
+
+  # Check the clustering performance is correct
+  melissa_obj <- eval_cluster_performance(melissa_obj, dt$opts$C_true)
+
+  expect_lt(melissa_obj$clustering$ari, 1.00001)
+  expect_gt(melissa_obj$clustering$ari, 0.99999)
+
+  expect_lt(melissa_obj$clustering$error, 0.001)
+  expect_gt(melissa_obj$clustering$error, -0.00001)
+
+
+  ##
+  # Test for Melissa Gibbs output
+  melissa_gibbs <- melissa_gibbs(X = dt$met, K = 2, basis = basis_obj,
+                               gibbs_nsim = 10, gibbs_burn_in = 5,
+                               is_parallel = FALSE, is_verbose = FALSE)
+
+  expect_lt(melissa_gibbs$summary$pi[1], 0.668493)
+  expect_gt(melissa_gibbs$summary$pi[1], 0.668492)
+
+  expect_lt(melissa_gibbs$r_nk[2,1], 1.001)
+  expect_gt(melissa_gibbs$r_nk[2,1], 0.999)
+
+  expect_lt(melissa_gibbs$W[1,1,1], 1.3791)
+  expect_gt(melissa_gibbs$W[1,1,1], 1.3790)
+})
